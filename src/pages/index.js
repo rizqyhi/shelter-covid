f590518317a097aa7b4342e6785f35ec89024648
Rizqy Hidayat <rizqy22@gmail.com>
2021-07-11 18:43:54 +0700

    Create base home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,82 +1,41 @@
 import Head from 'next/head'
+import HomeIllustration from '../images/home.svg'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <main className="p-6">
       <Head>
-        <title>Create Next App</title>
+        <title>Informasi Shelter Isolasi Covid-19</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <div className="py-8">
+        <HomeIllustration className="mx-auto" />
+      </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+      <h1 className="mb-4 text-2xl font-bold">
+        Temukan Tempat <span className="text-blue-500">Isolasi COVID-19</span><br />
+        di dekatmu
+      </h1>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+      <p className="mb-8 text-gray-400">
+        Temukan shelter untuk Isolasi Mandiri bagi pasien positif COVID-19 (dengan gejala ringan dan sedang).
+      </p>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+      <select className="w-full py-3 px-4 bg-gray-100 rounded-xl mb-4">
+        <option value="1">Yogyakarta</option>
+        <option value="1">Jawa Tengah</option>
+      </select>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+      <select className="w-full py-3 px-4 bg-gray-100 rounded-xl mb-4">
+        <option value="">Kota Yogyakarta</option>
+        <option value="">Sleman</option>
+        <option value="">Bantul</option>
+        <option value="">Kulonprogo</option>
+        <option value="">Gunungkidul</option>
+      </select>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+      <button className="w-full py-3 px-4 bg-blue-500 text-white text-center font-bold rounded-full">Cari Tempat Isolasi Terdekat</button>
+    </main>
   )
 }
